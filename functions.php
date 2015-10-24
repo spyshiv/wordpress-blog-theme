4d9f70fae4bc9ff4e6a1232cad495d5da831d828
Shiv Baran Singh <spyshiv@gmail.com>
2015-10-25 00:38:06 +0530

    added:basic setup inside functions.php and language translate page

---
+++ functions.php
@@ -0,0 +1,90 @@
+<?php 
+/**
+* This is the heart of theme. This file has all the integration with admin, widgets, static files, translater etc etc.
+* @package blogshiv 
+* for more details visit these links that will help you a lot in theme development
+* @link https://codex.wordpress.org/Theme_Development
+ * @link https://codex.wordpress.org/Child_Themes 
+*/
+
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which
+ * runs before the init hook. The init hook is too late for some features, such
+ * as indicating support for post thumbnails.
+ *
+ * @since blogshiv 1.0
+ */
+function blogshiv_setup(){
+
+	 /*
+	 * Make theme available for translation.
+	 * Translations can be filed in the /languages/ directory.
+	 */
+	 load_theme_textdomain( 'blogshiv', get_template_directory() . '/languages' );
+
+	// Add default posts and comments RSS feed links to head.
+	add_theme_support( 'automatic-feed-links' );
+
+	/*
+	 * Let WordPress manage the document title.
+	 * By adding theme support, we declare that this theme does not use a
+	 * hard-coded <title> tag in the document head, and expect WordPress to
+	 * provide it for us.
+	 */
+	add_theme_support( 'title-tag' );
+
+	/*
+	 * Enable support for Post Thumbnails on posts and pages.
+	 *
+	 * See: https://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
+	 */
+	add_theme_support( 'post-thumbnails' );
+	set_post_thumbnail_size( 825, 510, true );
+
+	// This theme uses wp_nav_menu() in two locations.
+	register_nav_menus( array(
+		'primary' => __( 'Primary Menu',      'blogshiv' ),
+		'social'  => __( 'Social Links Menu', 'blogshiv' ),
+	) );
+
+	/*
+	 * Switch default core markup for search form, comment form, and comments
+	 * to output valid HTML5.
+	 */
+	add_theme_support( 'html5', array(
+		'search-form', 'comment-form', 'comment-list', 'gallery', 'caption'
+	) );
+
+	/*
+	 * Enable support for Post Formats.
+	 *
+	 * See: https://codex.wordpress.org/Post_Formats
+	 */
+	add_theme_support( 'post-formats', array(
+		'aside', 'image', 'video', 'quote', 'link', 'gallery', 'status', 'audio', 'chat'
+	) );
+
+	$color_scheme  = twentyfifteen_get_color_scheme();
+	$default_color = trim( $color_scheme[0], '#' );
+
+	// Setup the WordPress core custom background feature.
+	add_theme_support( 'custom-background', apply_filters( 'blogshiv_custom_background_args', array(
+		'default-color'      => $default_color,
+		'default-attachment' => 'fixed',
+	) ) );
+
+	/*
+	 * This theme styles the visual editor to resemble the theme style,
+	 * specifically font, colors, icons, and column width.
+	 */
+	add_editor_style( array( 'css/editor-style.css', 'genericons/genericons.css', blogshiv_fonts_url() ) );
+}
+endif; 
+add_action( 'after_setup_theme', 'blogshiv_setup' );
+
+}
+
+?>
+
