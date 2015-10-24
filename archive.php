b46d853addac0cf6dd49222787d1e9fb647a0686
Shiv Baran Singh <spyshiv@gmail.com>
2015-10-25 01:26:23 +0530

    added:Content, comments. author, archive pages

--- archive.php
+++ archive.php
@@ -1,23 +1,35 @@
 <?php
 /**
-* Homepage Template
-* @package shiv
-*/
-?>
-<?php get_header(); ?>
-   <div id="primary" class="content-area">
+ * The template for displaying archive pages
+ *
+ * Used to display archive-type pages if nothing more specific matches a query.
+ * For example, puts together date-based pages if no date.php file exists.
+ *
+ * If you'd like to further customize these archive views, you may create a
+ * new template file for each one. For example, tag.php (Tag archives),
+ * category.php (Category archives), author.php (Author archives), etc.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+  * @package shiv
+ * @since blogshiv 1.0
+ */
+
+get_header(); ?>
+
+	<section id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
 		<?php if ( have_posts() ) : ?>
 
-			<?php if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-			<?php endif; ?>
+			<header class="page-header">
+				<?php
+					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_description( '<div class="taxonomy-description">', '</div>' );
+				?>
+			</header><!-- .page-header -->
 
 			<?php
-			// Start the loop.
+			// Start the Loop.
 			while ( have_posts() ) : the_post();
 
 				/*
@@ -45,5 +57,6 @@
 		?>
 
 		</main><!-- .site-main -->
-	</div><!-- .content-area -->
+	</section><!-- .content-area -->
+
 <?php get_footer(); ?>
---
+++ content-page.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The template used for displaying page content
+ *
+ * @package shiv
+ * @since blogshiv 1.0
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php
+		// Post thumbnail.
+		blogshiv_post_thumbnail();
+	?>
+
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages( array(
+				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'blogshiv' ) . '</span>',
+				'after'       => '</div>',
+				'link_before' => '<span>',
+				'link_after'  => '</span>',
+				'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'blogshiv' ) . ' </span>%',
+				'separator'   => '<span class="screen-reader-text">, </span>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<?php edit_post_link( __( 'Edit', 'blogshiv' ), '<footer class="entry-footer"><span class="edit-link">', '</span></footer><!-- .entry-footer -->' ); ?>
+
+</article><!-- #post-## -->
