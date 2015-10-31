49a59ee2b8aa06122d4711095d28ab97e3400aac
Shiv Baran Singh <spyshiv@gmail.com>
2015-11-01 00:22:55 +0530

    templates are added

---
+++ templates/blog-post.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Template Name: Blog Post
+ */
+
+	get_header();
+?>
+<h1>Welcome to blog-post page</h1>
+<?php
+	get_footer();?>
---
+++ templates/category-github.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Template Name: Category github
+ */
+
+	get_header();
+?>
+<h1>Welcome to github category page</h1>
+<?php
+	get_footer();?>
